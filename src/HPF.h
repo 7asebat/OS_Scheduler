4317bca29ccbdf2c3cf5a497c3c1549ee9c948cd
Youssef Walid <yousefwalid98@hotmail.com>
2020-12-26 22:03:02 +0200

    Added interface for HPF

---
+++ src/HPF.h
@@ -0,0 +1,21 @@
+#include "headers.h"
+
+void HPF_insertProcess(process *p)
+{
+}
+
+void HPF_updateState()
+{
+}
+
+char HPF_mustPreempt()
+{
+}
+
+process *HPF_getNextProcess()
+{
+}
+
+void HPF_removeProcess(process *p)
+{
+}
