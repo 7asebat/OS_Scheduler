cc8e5c69cda76c82cb35528128109601ba52a052
XL3 <abdelrahman.farid99@eng-st.cu.edu.eg>
2021-01-29 22:13:55 +0200

    More fixes to buddy allocation

--- src/buddy.h
+++ src/buddy.h
@@ -4,7 +4,7 @@
 #define __BUDDY_CAPACITY 1024
 
 struct {
-  char occupied[__BUDDY_CAPACITY];
+  bool occupied[__BUDDY_CAPACITY];
   short next[__BUDDY_CAPACITY];
 } __buddy_block;
 
@@ -34,12 +34,11 @@ int buddy_upperbound(int b) {
 int buddy_tryAllocate(int bytes) {
   // Get appropriate size
   int size = buddy_upperbound(bytes);
-
   int start = 0, next;
   int dmin = __INT_MAX__, minimum = __BUDDY_CAPACITY;
 
   while (start < __BUDDY_CAPACITY) {
-    // Find next available segment
+    // Find next available block
     if (__buddy_block.occupied[start]) {
       if (__buddy_block.next[start] % size) {
         start += size;
@@ -50,8 +49,8 @@ int buddy_tryAllocate(int bytes) {
       continue;
     }
 
-    // Found available segment
-    // Find minimum segment
+    // Found available block
+    // Find minimum block
     next = __buddy_block.next[start];
     if (next - start >= size && next - start < dmin) {
       dmin = next - start;
@@ -69,49 +68,62 @@ int buddy_tryAllocate(int bytes) {
  * @return index on success, -1 on failure
  */
 int buddy_allocate(int bytes) {
-  int size = buddy_upperbound(bytes);
   int minimum = buddy_tryAllocate(bytes);
+  if (minimum < 0) return -1;
 
-  if (minimum > -1) {
-    int next = __buddy_block.next[minimum];
+  // Get appropriate size
+  int size = buddy_upperbound(bytes);
 
-    if (minimum + size < __BUDDY_CAPACITY && !__buddy_block.occupied[minimum + size])
-      __buddy_block.next[minimum + size] = __buddy_block.next[minimum];  // Link with old next
+  // Make the next available block point to the following block
+  if (minimum + size < __BUDDY_CAPACITY &&
+      !__buddy_block.occupied[minimum + size])
+    __buddy_block.next[minimum + size] = min(__buddy_block.next[minimum], __BUDDY_CAPACITY);
 
-    __buddy_block.occupied[minimum] = 1;
-    __buddy_block.next[minimum] = minimum + size;
-    return minimum;
+  // Point to the next available block
+  __buddy_block.next[minimum] = minimum + size;
+  __buddy_block.occupied[minimum] = 1;
+
+  // Link previous smaller block to allocated block
+  int index = 0, next;
+  while (index < minimum) {
+    next = __buddy_block.next[index];
+    if (next >= minimum) {
+      __buddy_block.next[index] = minimum;
+      break;
+    }
+    index = next;
   }
-
-  return -1;
+  return minimum;
 }
 
-int buddy_free(int index, int bytes) {
+int buddy_free(int index) {
   if (!__buddy_block.occupied[index]) return -1;
 
   // Clear
   __buddy_block.occupied[index] = 0;
 
-  // Get appropriate size
-  int size = buddy_upperbound(bytes);
+  // Merge blocks iteratively, working up denominations
+  int size = __buddy_block.next[index] - index;
+  while (size < __BUDDY_CAPACITY) {
+    // Check for backward merge
+    // Move to occupied block
+    if (index / size % 2 &&
+        !__buddy_block.occupied[index - size]) index -= size;
 
-  // Check if mergable with next segment
-  if ((index / size % 2 == 0) && (!__buddy_block.occupied[index + size])) {
+    // Check if a merge is possible
+    if (__buddy_block.occupied[index + size] ||
+        __buddy_block.next[index] - index < size) break;
+
     // Link with next
     __buddy_block.next[index] = __buddy_block.next[index + size];
 
     // Erase next
     __buddy_block.next[index + size] = index + size + __BUDDY_CAPACITY;
-  }
-  // Check if mergable with previous segment
-  else if ((index >= size) && (!__buddy_block.occupied[index - size])) {
-    // Link with next
-    __buddy_block.next[index - size] = __buddy_block.next[index];
 
-    // Erase next
-    __buddy_block.next[index] = index + __BUDDY_CAPACITY;
+    size <<= 1;
   }
 
+  __buddy_block.next[index] = min(__buddy_block.next[index], __BUDDY_CAPACITY);
   return 0;
 }
 #endif
--- src/headers.h
+++ src/headers.h
@@ -25,6 +25,11 @@
        __typeof__ (b) _b = (b); \
      _a > _b ? _a : _b; })
 
+#define min(a, b) \
+  ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
 #define SHKEY 300
 
 #define STATUS_WAITING 0
